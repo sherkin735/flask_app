6f7719bfbd1bbc2fccaf88bdd6e8f720a0fe5e2b
William Norris <sherkin735@hotmail.com>
2015-07-19 19:07:07 +0100

    adding custom 404

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +7,10 @@ def hello():
 
 @app.errorhandler(404)
 def page_not_found(e):
+    return redirect(url_for('show_404'))
+
+@app.route('/not-found')
+def show_404():
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
